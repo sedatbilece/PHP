91ebf9c6d0294c0f8f6e2b190d5d617bed2525a5
Sedat Bilece <seda1tbilece@gmail.com>
2021-06-29 18:24:50 +0300

    datetime

---
+++ date-time-alma.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php 
+    # eğer hata alınırsa  bunu yap
+    #date_default_timezone_set("Europe");
+
+    echo date("d-m-y h:i:s");# 29-06-21 03:20:18
+    
+    echo" <p>----------------------------</p>";
+
+
+    
+    
+    ?>
+</body>
+</html>
