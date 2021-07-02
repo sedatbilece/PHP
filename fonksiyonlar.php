23358a5c81b567d2031dfb96bb2a956e38e51ebf
Sedat Bilece <seda1tbilece@gmail.com>
2021-07-02 22:39:15 +0300

    fonksiyonlar temel

---
+++ fonksiyonlar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php 
+
+
+function yaz (){
+
+
+    echo " fonksiyon yaz çağrıldı";
+}
+
+yaz();
+
+function topla($s1,$s2){
+
+    return $s1+$s2;
+}
+echo "</br>";
+
+echo topla(5,30);
+
+?>
+
+
+</body>
+</html>
