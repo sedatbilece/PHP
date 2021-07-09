7965fa8451c7dbdd67545917895bbf910317ccf0
sedatbilece <sedatb767@gmail.com>
2021-07-09 17:17:47 +0300

    pdo-insert

--- pdo-islemleri-giris/islem.php
+++ pdo-islemleri-giris/islem.php
@@ -1,17 +1,33 @@
 <?php
-
+require_once "baglan.php";
 
 $ad=$_POST["Ad"];
 $sifre=$_POST["Sifre"];
+
+echo "<p> $ad </p>";
+echo "<p> $sifre </p>";
+
 
 
 
 if(isset($_POST["girisform"])){
 
+    
+echo "<p>isset forma girildi </p>";
 
 
-echo  $ad;
-echo $sifre;
+$dbkaydet = $db->prepare("INSERT into name_pass set 
+
+ name=:a , pass=:b ");
+
+$insert = $kaydet ->execute(array(
+
+    "a"=> $_POST["Ad"],
+    "b"=> $_POST["Sifre"]
+
+));
+
+
 
 
 }
