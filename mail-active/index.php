eae915b4e17ad8ecc7664003f4859962e01de6a7
Sedat Bilece <seda1tbilece@gmail.com>
2021-07-22 12:33:13 +0300

    aktivasyon kod deneme

---
+++ mail-active/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+        .aralik{
+
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    
+
+
+
+<form action="islem.php" method="post">
+
+
+<input type="text" name="isim" placeholder="isim gir" class="aralik">
+<br>
+<input type="mail" name="eposta" placeholder="eposta gir" class="aralik">
+<br>
+<input type="password" name="sifre" placeholder="sifre gir" class="aralik">
+<br>
+<input type="submit" value="Kayıt" class="aralik" name="kayitbutonu">
+
+</form>
+
+
+</body>
+</html>
