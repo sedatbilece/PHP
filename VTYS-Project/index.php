7bca50b05bdebfd1d82716c5c5fd9c160ba6c8ef
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-12-08 16:56:30 +0300

    proje oluşturuldu

---
+++ VTYS-Project/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anasayfa</title>
+    <link href ="tailwind.css">
+    
+<style>
+    @import url(https://fonts.googleapis.com/css?family=Roboto);
+@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css);
+
+* {
+  font-family: Roboto;
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+  overflow-x: hidden;
+}
+
+  .sidenav {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    height: 100%;
+    width: 60px;
+    background: #333;
+  }
+  
+  
+    .sidenav .nav {
+      position: relative;
+      display: flex;
+      align-items: center;
+      z-index: 1000;
+      left: 0px;
+      transition: all 0.4s ease;
+      cursor: pointer;
+    }
+
+    .sidenav .nav.active {
+      left: -10px;
+    }
+
+    .sidenav .nav:hover {
+      left: -85px;      /* Border width*2 */
+    }
+
+      .sidenav .description, .sidenav .icon {
+        height: 60px;
+        line-height: 60px;
+        background: #333;
+      }
+
+      .sidenav .nav.active .description, .sidenav .nav.active .icon {
+        background: #FF5722;
+      }
+
+      .sidenav .icon {
+        width: 60px;          /* Width of navbar */
+        text-align: center;
+        color: #e9e9e9;
+        font-size: 25px;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+      }
+
+      .sidenav .description {
+        position: relative;
+        left: 60px;
+        color: #eee;
+        min-width: 80px;
+        padding-left: 20px;
+        height: 100%;
+      }
+
+  .content {
+    position: absolute;
+    left: 0px;
+    padding-left: 40px;
+    padding-top: 40px;
+    top: 0px;
+    right: 60px;        /* Width of navbar */
+    height: 100%;
+    z-index: 10;
+    background: #F7F5F5;
+  }
+</style>
+</head>
+<body>
+
+<div class="sidenav right">
+  <div class="nav active"><div class="icon fa fa-home"></div><div class="description">Anasayfa</div></div>
+  <div class="nav"><div class="icon fa fa-database"></div><div class="description">About</div></div>
+  <div class="nav "><div class="icon fa fa-users"></div><div class="description">Clients</div></div>
+</div>
+
+<div class="content">
+  
+
+asd
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
