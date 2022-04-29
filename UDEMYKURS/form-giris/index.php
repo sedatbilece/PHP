cd3b0cb3ff964c3fa49d85d1961c0cf37a1eff38
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-04-29 23:23:02 +0300

    form-get-post

---
+++ UDEMYKURS/form-giris/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="./islem.php" method="post">
+   isim Soyisim <br>
+   <input type="text" name="adsoyad"><br>
+   yaş<br>
+   <input type="number" name="yas" id=""><br>
+   <button type="submit" name="isimform">Gönder</button>
+
+
+</form>
+
+
+</body>
+</html>
---
+++ UDEMYKURS/form-giris/islem.php
@@ -0,0 +1,12 @@
+
+
+<?php 
+
+if(isset($_POST["isimform"])){
+
+
+    echo $_POST["adsoyad"];echo "<br>";
+    echo $_POST["yas"];echo "<br>";
+}
+
+?>
