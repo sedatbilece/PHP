64ad828726a16cfe5759c4cc6a41b97bb08eecf5
sedatbilece <sedatb767@mail.com>
2022-06-26 14:32:51 +0300

    route yapısı

--- LaravelProjects/first-app/routes/web.php
+++ LaravelProjects/first-app/routes/web.php
@@ -13,6 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("basics")->group(function(){// /basics/yapi şeklinde gruplamak için kullanılır
+
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    
+    Route::get('/merhaba', function () {
+        return "Hello from API";
+    });
+    
+    Route::get('/merhaba-json', function () {
+        return ["message"=>"HELLO SWEATHEART","message2"=>"welcome to laravel api",["listtolist"=>"deneme"]];
+    });
+    
+    Route::get('/merhaba-json2', function () {
+        return response( ["message"=>"HELLO SWEATHEART"],200)->content('Content-type','text/plain') ;
+    });
+    Route::get('/merhaba-json3', function () {
+        return response()->json(["message"=>"HELLO SWEATHEART"]);
+    });
+    
+    Route::get('/product/{id?}', function ($id=null) {
+                   if($id==null){
+                    return "Parametre verilmedi !!! ";
+                   }
+                  return "Product id: $id "; 
+    });
+    
+    Route::get('/category/{id}', function ($id=null) {
+        
+       return "category id : $id";
+    })->name("category.show");//url redirect için adı
+    
+    
+    Route::get('/categorygetir', function () {//categorygetir deyince redirecct ile verilen addaki urle gidilecek 
+        
+        return redirect()->route("category.show",["id"=>21]);
+     });
+
+     
+
 });
+
